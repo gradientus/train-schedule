0a1b577fab38f4565d1ac82ebdd72c13590869b7
Phil Mauracher <gradientus@gmail.com>
2019-07-30 13:28:56 -0600

    I am connected to the Firebase database and can display dummy data in the database on screen.

---
+++ assets/javascript/trains.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyDwV6Ai3Eb8YvD_iCDj6QpdQO5vTQE8qhY",
+    authDomain: "trainschedule-3055b.firebaseapp.com",
+    databaseURL: "https://trainschedule-3055b.firebaseio.com",
+    projectId: "trainschedule-3055b",
+    storageBucket: "",
+    messagingSenderId: "22705671889",
+    appId: "1:22705671889:web:6de3806fa99e76ee"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  //when the user clicks the submit button it grabs the values in the input fields
+  $("#submit").on("click", function() {
+    event.preventDefault();
+    var train = $("#train")
+      .val()
+      .trim();
+    var destination = $("#destination")
+      .val()
+      .trim();
+    var departure = $("#departure")
+      .val()
+      .trim();
+    var frequency = $("#frequency")
+      .val()
+      .trim();
+  });
+
+  database.ref().on(
+    "value",
+    function(snapshot) {
+      console.log(snapshot.val());
+      train = snapshot.val().train;
+      destination = snapshot.val().destination;
+      departure = snapshot.val().departure;
+      frequency = snapshot.val().frequency;
+      $("#scheduleInfo").text(
+        `${train}    ${destination}    ${departure}   ${frequency}`
+      );
+    },
+    function(errorObject) {
+      console.log("Something bad happened: " + errorObject.code);
+    }
+  );
+});
