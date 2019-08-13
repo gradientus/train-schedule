1e0f9040f4b1f02350f41c6257044c86822604df
Phil Mauracher <gradientus@gmail.com>
2019-08-12 23:09:59 -0600

    Fixed the time calculation problem

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,80 +1,99 @@
-//setup Firebase connection information
-var firebaseConfig = {
-  apiKey: "AIzaSyDwV6Ai3Eb8YvD_iCDj6QpdQO5vTQE8qhY",
-  authDomain: "trainschedule-3055b.firebaseapp.com",
-  databaseURL: "https://trainschedule-3055b.firebaseio.com",
-  projectId: "trainschedule-3055b",
-  storageBucket: "",
-  messagingSenderId: "22705671889",
-  appId: "1:22705671889:web:6de3806fa99e76ee"
-};
-
-//
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-
-//
-//click submit to be able to add underground line info
-$("#submit").on("click", function() {
-  event.preventDefault();
-  let train = $("#train")
-    .val()
-    .trim();
-  let destination = $("#destination")
-    .val()
-    .trim();
-  let departure = $("#departure")
-    .val()
-    .trim();
-  let frequency = $("#frequency")
-    .val()
-    .trim();
-
-  //
-  //to hold then push the underground line information
-  var undObj = {
-    train: train,
-    destination: destination,
-    departure: departure,
-    frequency: frequency
+$(document).ready(function() {
+  //setup Firebase connection information
+  var firebaseConfig = {
+    apiKey: "AIzaSyDwV6Ai3Eb8YvD_iCDj6QpdQO5vTQE8qhY",
+    authDomain: "trainschedule-3055b.firebaseapp.com",
+    databaseURL: "https://trainschedule-3055b.firebaseio.com",
+    projectId: "trainschedule-3055b",
+    storageBucket: "",
+    messagingSenderId: "22705671889",
+    appId: "1:22705671889:web:6de3806fa99e76ee"
   };
 
   //
-  //to push the underground line information
-  database.ref().push(undObj);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
   //
-  //to clear the cell values after submit was clicked
-  clearValues();
-});
+  //click submit to be able to add underground line info
+  $("#submit").on("click", function() {
+    event.preventDefault();
+    let train = $("#train")
+      .val()
+      .trim();
+    let destination = $("#destination")
+      .val()
+      .trim();
+    let departure = $("#departure")
+      .val()
+      .trim();
+    let frequency = $("#frequency")
+      .val()
+      .trim();
 
-//
-// function to clear cell values
-function clearValues() {
-  $("#train").val("");
-  $("#destination").val("");
-  $("#departure").val("");
-  $("#frequency").val("");
-}
+    //
+    //to hold then push the underground line information
+    var undObj = {
+      train: train,
+      destination: destination,
+      departure: departure,
+      frequency: frequency
+    };
 
-//
-//to reflect the information in the database in a tr and td elements
-database.ref().on("child_added", function(childSnapshot) {
-  var tableTrain = childSnapshot.val().train;
-  var tableDest = childSnapshot.val().destination;
-  var tableDep = childSnapshot.val().departure;
-  var tableFreq = childSnapshot.val().frequency;
+    //
+    //to push the underground line information
+    database.ref().push(undObj);
+
+    //
+    //to clear the cell values after submit was clicked
+    clearValues();
+  });
 
   //
-  //to append the info from Firebase to the table
-  $("#scheduleInfo").append(
-    `<tr class="trainRow"><td class="trainCell">${tableTrain}</td><td class="trainCell">${tableDest}</td><td class="trainCell">${tableFreq}</td><td class="trainCell">14:00</td><td class="trainCell">19</td></tr>`
-  );
-});
+  // function to clear cell values after submit because they stick around
+  function clearValues() {
+    $("#train").val("");
+    $("#destination").val("");
+    $("#departure").val("");
+    $("#frequency").val("");
+  }
 
-//TODO: wrap everything in .ready overall function
-//TODO: use the moment.js thing to convert dates
-//TODO: use math to calculate when the next train arrives
-//TODO: use math to calculate how many minutes away that is from right now
-//TODO: add a button to reset the whole database
+  //
+  //event listener declaring variables to hold the information of the most recent child added to the database
+  database.ref().on("child_added", function(childSnapshot) {
+    var tableTrain = childSnapshot.val().train;
+    var tableDest = childSnapshot.val().destination;
+    var tableDep = childSnapshot.val().departure; //first time
+    var tableFreq = childSnapshot.val().frequency; //frequency
+
+    //
+    //variables and math to calculate the next arrival time and how many minutes away the train is
+    var now = moment(); //capture current time
+    //console.log(now);
+
+    var timeLeave = moment(tableDep, "HH:mm"); //capture the first departure time from Firebase
+    //console.log(timeLeave);
+
+    var difference = now.diff(timeLeave, "minutes"); //capture how many minutes between now and Firebase time
+    //console.log(difference);
+
+    var mod = difference % tableFreq; //capture the number of minutes between the difference and the frequency
+    //console.log(mod);
+
+    var minutesAway = tableFreq - mod;
+    //console.log(minutesAway);
+
+    var nextArrival = now.add(minutesAway, "minutes").format("HH:mm");
+    //console.log(nextArrival);
+
+    //
+    //to append the info from Firebase and the time calculations to the table
+    $("#scheduleInfo").append(
+      `<tr class="trainRow"><td class="trainCell">${tableTrain}</td><td class="trainCell">${tableDest}</td><td class="trainCell">${tableFreq}</td><td class="trainCell">${nextArrival}</td><td class="trainCell">${minutesAway}</td></tr>`
+    );
+  });
+
+  //ICEBOX: add a button to delete the last entry
+  //ICEBOX: add a button to reset the whole database
+});
